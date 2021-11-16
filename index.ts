8fef856cb3c3c44440dc76f066c4b2bf0642094a
Simon <simon.cheng@advantech.com.tw>
2021-11-16 17:14:38 +0800

    Export i18n types

--- index.ts
+++ index.ts
@@ -2,4 +2,4 @@ export { store_json } from "./lib/functions/store-json.functions";
 export { get } from "./lib/functions/get.functions";
 export { find } from "./lib/functions/find.functions";
 export { sort } from "./lib/functions/sort.functions";
-export { asc_type } from "./lib/interfaces/asc.interface";
+export { asc_type, i18n_type } from "./lib/interfaces/asc.interface";

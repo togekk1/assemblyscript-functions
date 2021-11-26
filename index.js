279ee86f1b6b5cdcd0b535bc6594266144c3ea75
Simon <simon.cheng@advantech.com.tw>
2021-11-26 11:24:39 +0800

    Add find_index export

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 export { store_json } from "./lib/functions/store-json.functions";
 export { get } from "./lib/functions/get.functions";
-export { find } from "./lib/functions/find.functions";
+export { find, find_index } from "./lib/functions/find.functions";
 export { sort } from "./lib/functions/sort.functions";
--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 export { store_json } from "./lib/functions/store-json.functions";
 export { get } from "./lib/functions/get.functions";
-export { find } from "./lib/functions/find.functions";
+export { find, find_index } from "./lib/functions/find.functions";
 export { sort } from "./lib/functions/sort.functions";
 export { asc_type, i18n_type } from "./lib/interfaces/asc.interface";

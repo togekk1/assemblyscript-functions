eae5e577a3208688f0c6233fcb037fdbbcf77355
Simon <simon.cheng@advantech.com.tw>
2022-02-09 17:14:23 +0800

    Fix import paths in commonjs version of index.js

--- index.js
+++
@@ -1,4 +0,0 @@
-export { store_json } from "./lib/functions/store-json.functions";
-export { get } from "./lib/functions/get.functions";
-export { find, find_index } from "./lib/functions/find.functions";
-export { sort } from "./lib/functions/sort.functions";

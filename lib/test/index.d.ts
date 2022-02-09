6f0b62438fb606677c7668cb77df6596feb8a78b
Simon <simon.cheng@advantech.com.tw>
2022-02-09 18:01:45 +0800

    Use path alias for imports

--- lib/test/index.d.ts
+++ lib/test/index.d.ts
@@ -1,5 +1,5 @@
-export { store_json } from '../lib/functions/store-json.functions';
-export { get } from '../lib/functions/get.functions';
-export { find, find_index } from '../lib/functions/find.functions';
-export { sort } from '../lib/functions/sort.functions';
-export { asc_type, i18n_type } from '../lib/interfaces/asc.interface';
+export { store_json } from 'functions/store-json.functions';
+export { get } from 'functions/get.functions';
+export { find, find_index } from 'functions/find.functions';
+export { sort } from 'functions/sort.functions';
+export { asc_type, i18n_type } from 'interfaces/asc.interface';

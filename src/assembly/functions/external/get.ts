442355a6575505c717894e255e366f1564dd37c9
Simon <simon.cheng@advantech.com.tw>
2022-03-30 14:45:33 +0800

    Use static arrays instead of converted strings

--- src/assembly/functions/external/get.ts
+++ src/assembly/functions/external/get.ts
@@ -5,10 +5,10 @@ import { data } from '../..';
 // let keys_global: string[];
 // let indexes_global: Int32Array;
 // let list: JSON.Arr | null;
-let get_interger_global: ((arr: JSON.Arr | null, keys: string[], indexes: Int32Array, level: i32) => i64) | null;
-let get_float_global: ((arr: JSON.Arr | null, keys: string[], indexes: Int32Array, level: i32) => f64) | null;
-let get_string_global: ((arr: JSON.Arr | null, keys: string[], indexes: Int32Array, level: i32) => string | null) | null;
-let get_boolean_global: ((arr: JSON.Arr | null, keys: string[], indexes: Int32Array, level: i32) => bool) | null;
+let get_interger_global: ((arr: JSON.Arr | null, keys: StaticArray<string>, indexes: Int32Array, level: i32) => i64) | null;
+let get_float_global: ((arr: JSON.Arr | null, keys: StaticArray<string>, indexes: Int32Array, level: i32) => f64) | null;
+let get_string_global: ((arr: JSON.Arr | null, keys: StaticArray<string>, indexes: Int32Array, level: i32) => string | null) | null;
+let get_boolean_global: ((arr: JSON.Arr | null, keys: StaticArray<string>, indexes: Int32Array, level: i32) => bool) | null;
 
 /**
  * Get the integer value from the item of the list
@@ -16,11 +16,10 @@ let get_boolean_global: ((arr: JSON.Arr | null, keys: string[], indexes: Int32Ar
  * @param index Item index number in the list
  * @param key Property key in the Object to query
  */
-export function get_integer(list_name: string, indexes: Int32Array, keys: string | null): i64 {
+export function get_integer(list_name: string, indexes: Int32Array, keys: StaticArray<string> | null): i64 {
   const list = data.getArr(list_name);
   if (keys) {
-    const keys_new = (<JSON.Arr>JSON.parse(keys)).valueOf().map<string>((value: JSON.Value) => value.toString());
-    get_interger_global = (arr: JSON.Arr | null, keys: string[], indexes: Int32Array, level: i32): i64 => {
+    get_interger_global = (arr: JSON.Arr | null, keys: StaticArray<string>, indexes: Int32Array, level: i32): i64 => {
       if (arr && arr.valueOf().length) {
         const index = indexes[level];
         const data_value = arr.valueOf()[index];
@@ -37,7 +36,7 @@ export function get_integer(list_name: string, indexes: Int32Array, keys: string
       } else return 0;
     };
 
-    const value = get_interger_global(list, keys_new, indexes, 0);
+    const value = get_interger_global(list, keys, indexes, 0);
 
     get_interger_global = null;
     return value;
@@ -50,11 +49,10 @@ export function get_integer(list_name: string, indexes: Int32Array, keys: string
  * @param index Item index number in the list
  * @param key Property key in the Object to query
  */
-export function get_float(list_name: string, indexes: Int32Array, keys: string | null): f64 {
+export function get_float(list_name: string, indexes: Int32Array, keys: StaticArray<string> | null): f64 {
   const list = data.getArr(list_name);
   if (keys) {
-    const keys_new = (<JSON.Arr>JSON.parse(keys)).valueOf().map<string>((value: JSON.Value) => value.toString());
-    get_float_global = (arr: JSON.Arr | null, keys: string[], indexes: Int32Array, level: i32): f64 => {
+    get_float_global = (arr: JSON.Arr | null, keys: StaticArray<string>, indexes: Int32Array, level: i32): f64 => {
       if (arr && arr.valueOf().length) {
         const index = indexes[level];
         const data_value = arr.valueOf()[index];
@@ -71,7 +69,7 @@ export function get_float(list_name: string, indexes: Int32Array, keys: string |
       } else return 0;
     };
 
-    const value = get_float_global(list, keys_new, indexes, 0);
+    const value = get_float_global(list, keys, indexes, 0);
 
     get_float_global = null;
     return value;
@@ -84,12 +82,10 @@ export function get_float(list_name: string, indexes: Int32Array, keys: string |
  * @param index Item index number in the list
  * @param key Property key in the Object to query
  */
-export function get_string(list_name: string, indexes: Int32Array, keys: string | null, ellipsis_length: i32 = 0): string | null {
+export function get_string(list_name: string, indexes: Int32Array, keys: StaticArray<string> | null, ellipsis_length: i32 = 0): string | null {
   const list = data.getArr(list_name);
   if (keys) {
-    const keys_new = (<JSON.Arr>JSON.parse(keys)).valueOf().map<string>((value: JSON.Value) => value.toString());
-
-    get_string_global = (arr: JSON.Arr | null, keys: string[], indexes: Int32Array, level: i32): string | null => {
+    get_string_global = (arr: JSON.Arr | null, keys: StaticArray<string>, indexes: Int32Array, level: i32): string | null => {
       if (arr && arr.valueOf().length) {
         const index = indexes[level];
 
@@ -110,20 +106,11 @@ export function get_string(list_name: string, indexes: Int32Array, keys: string 
       } else return null;
     };
 
-    const value_string = get_string_global(list, keys_new, indexes, 0);
+    const value_string = get_string_global(list, keys, indexes, 0);
     get_string_global = null;
     return value_string ? (ellipsis_length && value_string.length > ellipsis_length ? `${value_string.substr(0, ellipsis_length)}...` : value_string) : null;
   } else return list ? (<JSON.Str>(<JSON.Arr>list).valueOf()[indexes[0]]).valueOf() : null;
 }
-// export function get_string(list_name: string, index: i32, key: string): string | null {
-//   const arr = data.getArr(list_name);
-//   if (arr) {
-//     const data_value = arr.valueOf()[index];
-//     const data_to_query: JSON.Obj = changetype<JSON.Obj>(data_value);
-//     const value: JSON.Str | null = data_to_query.getString(key);
-//     return value ? value.valueOf() : null;
-//   } else return null;
-// }
 
 /**
  * Get the boolean value from the item of the list
@@ -131,11 +118,10 @@ export function get_string(list_name: string, indexes: Int32Array, keys: string 
  * @param index Item index number in the list
  * @param key Property key in the Object to query
  */
-export function get_boolean(list_name: string, indexes: Int32Array, keys: string | null): bool {
+export function get_boolean(list_name: string, indexes: Int32Array, keys: StaticArray<string> | null): bool {
   const list = data.getArr(list_name);
   if (keys) {
-    const keys_new = (<JSON.Arr>JSON.parse(keys)).valueOf().map<string>((value: JSON.Value) => value.toString());
-    get_boolean_global = (arr: JSON.Arr | null, keys: string[], indexes: Int32Array, level: i32): bool => {
+    get_boolean_global = (arr: JSON.Arr | null, keys: StaticArray<string>, indexes: Int32Array, level: i32): bool => {
       if (arr && arr.valueOf().length) {
         const index = indexes[level];
         const data_value = arr.valueOf()[index];
@@ -153,31 +139,8 @@ export function get_boolean(list_name: string, indexes: Int32Array, keys: string
       } else return false;
     };
 
-    const value = get_boolean_global(list, keys_new, indexes, 0);
-    // level = 0;
-    // list = null;
-    // keys_global = [];
-    // indexes_global = new Int32Array(0);
+    const value = get_boolean_global(list, keys, indexes, 0);
     get_string_global = null;
     return value;
   } else return list ? (<JSON.Bool>(<JSON.Arr>list).valueOf()[indexes[0]]).valueOf() : false;
 }
-
-// export function get_boolean(list_name: string, index: i32, key: string): bool {
-//   const arr = data.getArr(list_name);
-//   if (arr) {
-//     const data_value = arr.valueOf()[index];
-//     const data_to_query: JSON.Obj = changetype<JSON.Obj>(data_value);
-//     const value: JSON.Bool | null = data_to_query.getBool(key);
-//     return value ? value.valueOf() : false;
-//   } else return false;
-// }
-
-// function get_array(arr: JSON.Value, index: i32, key: string): string[] | null {
-//   const data_value = (<JSON.Arr>arr).valueOf()[index];
-//   const data_to_query: JSON.Obj = changetype<JSON.Obj>(data_value);
-//   const value_json: JSON.Arr | null = data_to_query.getArr(key);
-//   const values = value_json ? value_json.valueOf() : null;
-//   const a = values ? values.map<string>((value: JSON.Value): string => value.toString()) : null;
-//   return a;
-// }
